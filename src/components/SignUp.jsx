a28d744b80a1f3b6c4d811da17a3fc6735afced0
neha pradhan <neha.pradhan@fusemachines.com>
2017-06-19 15:43:03 +0545

    auth and firebase

--- src/components/SignUp.jsx
+++ src/components/SignUp.jsx
@@ -1,5 +1,27 @@
 import React,{Component} from 'react';
+import {firebaseApp} from '../firebase';
  class SignUp extends Component{
+
+   constructor(props){
+     super(props);
+     this.state={
+       email:'',
+       password:'',
+       error:{
+         message:''
+       }
+     }
+   }
+
+  signUp(){
+    console.log("this.state>>>>",this.state);
+    const {email, password}=this.state;
+    firebaseApp.auth().createUserWithEmailAndPassword(email,password)
+    .catch(error=>{
+      console.log("error>>>",error);
+      this.setState({error});
+    })
+  }
    render(){
      return(
        <div className="form-inline">
@@ -7,15 +29,20 @@ import React,{Component} from 'react';
        <div className="form-group">
        <input className="form-control"
        type="text"
-       placeholder="email"/><br/>
+       placeholder="email"
+       onChange={event=>this.setState({email:event.target.value})}/><br/>
        <input className="form-control"
        type="password"
-       placeholder="password" /><br/>
+       placeholder="password"
+       onChange={event=> this.setState({password:event.target.value})}
+        /><br/>
 
-       <button className="btn btn-primary">
+       <button className="btn btn-primary" onClick={()=> this.signUp()}>
        Sign Up
        </button>
        </div>
+
+        <div>{this.state.error.message}</div>
         </div>
      );
    }

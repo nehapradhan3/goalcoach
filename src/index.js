58d3b9112451dbe4f18bca2b0cc2f114f51429f4
neha pradhan <neha.pradhan@fusemachines.com>
2017-06-21 17:25:49 +0545

    redux

--- src/index.js
+++ src/index.js
@@ -2,14 +2,21 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { Router,Route, browserHistory} from 'react-router';
 import {firebaseApp} from './firebase';
+import {Provider} from 'react-redux';
+import {createStore} from 'redux';
+import reducer from './reducers';
+import {logUser} from './actions';
 
 import App from './components/App';
 import SignIn from './components/SignIn';
 import SignUp from './components/SignUp';
 
+const store = createStore(reducer)
 firebaseApp.auth().onAuthStateChanged(user=>{
   if (user){
     console.log('user has signed in or up',user);
+    const {email}=user;
+    store.dispatch(logUser(email));
     browserHistory.push('/app');
   }else {
     console.log('user is signed out or still needs to sign in');
@@ -18,11 +25,13 @@ firebaseApp.auth().onAuthStateChanged(user=>{
 })
 
 ReactDOM.render(
+  <Provider store={store}>
   <Router path="/" history={browserHistory}>
   <Router path="/app" component={App} />
   <Router path="/signin" component={SignIn} />
   <Router path="/signup" component={SignUp} />
 
 
-  </Router>,document.getElementById('root')
+  </Router>
+  </Provider>,document.getElementById('root')
 )
